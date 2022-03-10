b28c3181404c5298bc2fb7c606c28fb2396ef6f1
mgoldp <mgoldp2013@gmail.com>
2022-03-10 17:21:01 -0300

    Separando la lógica en varios archivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:counter_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        home: Center(
-      child: Text('Hola Mundo'),
-    ));
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }

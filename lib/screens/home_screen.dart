dbe8591aacc85fd69ecb6fd2ff0598a28c567de4
mgoldp <mgoldp2013@gmail.com>
2022-03-11 19:52:52 -0300

    Iconos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,24 +8,30 @@ class HomeScreen extends StatelessWidget {
     const TextStyle fontSize30 = TextStyle(fontSize: 30);
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('HomeScreen'),
-          elevation: 0.0,
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Número de clicks:',
+              style: fontSize30,
+            ),
+            Text(
+              '10',
+              style: fontSize30,
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                'Número de clicks:',
-                style: fontSize30,
-              ),
-              Text(
-                '10',
-                style: fontSize30,
-              ),
-            ],
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            print('Hola Mundo');
+          }),
+    );
   }
 }
